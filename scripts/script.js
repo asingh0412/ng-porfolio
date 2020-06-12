85b9a7b1260277c511611d291edeaca136dfb599
Amaninder <asingh0412@yahoo.comgit config --global user.email asingh0412@yahoo.com>
2020-06-12 11:52:52 -0400

    Added new fucntionality for test

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // create the module and name it scotchApp
-var scotchApp = angular.module('scotchApp', ['ngRoute']);
+var scotchApp = angular.module('scotchApp', ['ngRoute','ui.bootstrap', 'utils.autofocus']);
 
 // configure our routes
 scotchApp.config(function($routeProvider) {
@@ -28,6 +28,11 @@ scotchApp.config(function($routeProvider) {
 scotchApp.controller('mainController', function($scope) {
   // create a message to display in our view
   $scope.message = 'Everyone come and see how good I look!';
+  $scope.format = 'yyyy/dd/MM';
+  $scope.date = new Date();
+
+
+
 });
 
 scotchApp.controller('aboutController', function($scope) {
@@ -39,19 +44,19 @@ scotchApp.controller('contactController', function($scope) {
 
   console.log('Contact Controller');
   $scope.states=[
-    {id: 0, name:" "},
-    {id: 1, name:"Washington"},
-    {id: 2, name:"Arizona"},
-    {id: 3, name:"California"},
-    {id: 4, name:"Virginia"},
-    {id: 5, name:"Michigan"},
-    {id: 6, name:"New York"},
-    {id: 7, name:"New Mexico"},
-    {id: 8, name:"Nevada"},
-    {id: 9, name:"Utah"},
-    {id: 10, name:"Wyoming"},
-    {id: 11, name:"West Virginia"},
-    {id: 12, name:"Oregon"}
+      {id: 0, name:" "},
+      {id: 1, name:"Washington"},
+      {id: 2, name:"Arizona"},
+      {id: 3, name:"California"},
+      {id: 4, name:"Virginia"},
+      {id: 5, name:"Michigan"},
+      {id: 6, name:"New York"},
+      {id: 7, name:"New Mexico"},
+      {id: 8, name:"Nevada"},
+      {id: 9, name:"Utah"},
+      {id: 10, name:"Wyoming"},
+      {id: 11, name:"West Virginia"},
+      {id: 12, name:"Oregon"}
     ];
 
     //This sets the default value of the select in the ui
@@ -59,4 +64,33 @@ scotchApp.controller('contactController', function($scope) {
     var s = $scope.selectedState.name;
     console.log(s);
 
+  // attach original to the object
+  $scope.original={
+    fName:'A',
+    lName:'S',
+    gender:'male',
+    email:'as@as.com',
+    address:'1st st, T3',
+    city:'abc',
+    states:'Virginia',
+    zip:'12345'
+  };
+
+  //Copy the original to contact. contact will be bind to a form.
+  $scope.contact = angular.copy($scope.orignial);
+
+  $scope.resetForm = function (){
+    //alert($scope.contact.fName);
+      //$scope.contact = [];
+      $scope.contact = angular.copy($scope.original);
+  };
+
+  $scope.format = 'MM/dd/yyyy';
+  $scope.date = new Date();
 });
+function randomFunc(){
+  console.log("In Random function");
+  var num1 = parseInt(document.getElementById("rand1").value);
+  var num2 = parseInt(document.getElementById("rand2").value);
+  return Math.floor(Math.random() * (num2-num1)) + num1;
+}
